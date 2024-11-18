5540c7ddc6a2828ceee2468be54dc74f9f7a68a0
Mr. Kaehms <kaehmsrobert@dublinusd.org>
2024-11-17 22:50:01 -0800

    Update show-system.sh
    added hostname and ip address

--- sys/scripts/show-system.sh
+++ sys/scripts/show-system.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+
+echo -n "HOSTNAME: "
+hostname
+
+echo -n "IP: "
+ifconfig | grep netmask | grep broadcast
+
 # Display the OS
 cat /etc/os-release | grep PRETTY | perl -p -i -e 's/PRETTY/OS/g' 2>/dev/null
 

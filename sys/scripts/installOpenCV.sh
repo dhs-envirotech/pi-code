5e2f8a6ab1caecb40c8c2fa25c78747085366fbf
Mr. Kaehms <kaehmsrobert@dublinusd.org>
2024-11-19 09:04:47 -0800

    Update installOpenCV.sh
    Added some notes on install

--- sys/scripts/installOpenCV.sh
+++ sys/scripts/installOpenCV.sh
@@ -1,2 +1,6 @@
-# verify this - may be python3-opencv
+# from command line
 apt get-install python-opencv
+
+# When I installed, I had not updated the OK so had to run this after initial install failed
+# sudo apt --fix-missing update
+# udo apt-get -f python-opencv  (NOTE: -f force the installation of the broken packages)

21416c8781a1fb351c17151fbe274031682803b4
Mr. Kaehms <kaehmsrobert@dublinusd.org>
2024-11-19 12:43:40 -0800

    Update installOpenCV.sh

--- sys/scripts/installOpenCV.sh
+++ sys/scripts/installOpenCV.sh
@@ -1,5 +1,6 @@
 # from command line
-apt-geti install python-opencv
+apt-get install python-opencv
+# NOTE: Above worked for 64bit raspbien. For 32bit, use apt-get install python3-opencv
 
 # When I installed, I had not updated the OS so had to run this after initial install failed
 # sudo apt --fix-missing update
